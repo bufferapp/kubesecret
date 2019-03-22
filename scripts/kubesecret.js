6ccfb8888fedf3d063b4eee15f9fd0eba72733b3
Adnan Issadeen <eddyrox1@hotmail.com>
2019-03-22 19:08:45 +0530

    Final help message before MVP release

--- scripts/kubesecret.js
+++ scripts/kubesecret.js
@@ -4,5 +4,6 @@ process.env.NODE_PATH = `${__dirname}/../node_modules`;
 
 program.version('0.1.0');
 program.name('kubesecret');
+program.usage('\nGet started by running ./kubesecret get -n <namespace>');
 program.command('get', 'Starts the process of getting secret(s) and performing operations on it');
 program.parse(process.argv);
