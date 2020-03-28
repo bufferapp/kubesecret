6b1136cc1816b2f9558ee51e3b9797aeffc18b2d
Dan Farrelly <daniel.j.farrelly@gmail.com>
2020-03-28 12:53:17 -0400

    Add error handling for invalid secret name

--- scripts/kubesecret-get.js
+++ scripts/kubesecret-get.js
@@ -1,15 +1,21 @@
 const program = require('commander');
 const interactiveget = require('../lib/interactive/retrieve');
+const { red } = require('chalk');
 
 program.name('kubesecret get');
 program
   .option('-n --namespace <namespace>', 'Namespace to filter by')
   .arguments('[secret-name]')
-  .action((secretName) => {
-    interactiveget.beginWithNamespace({
-      namespace: program.namespace,
-      secretName,
-    });
+  .action(async (secretName) => {
+    try {
+      await interactiveget.beginWithNamespace({
+        namespace: program.namespace,
+        secretName,
+      });
+    } catch (err) {
+      console.log(red(`${err.message}`));
+      process.exit(1);
+    }
   });
 
 program.parse(process.argv);
