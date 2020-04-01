99c8861047aff7ff703a7965887bceb5e8eb3eef
Adnan Issadeen <eddyrox1@hotmail.com>
2020-04-01 11:56:30 +0000

    Bumped program version

--- scripts/kubesecret.js
+++ scripts/kubesecret.js
@@ -3,7 +3,7 @@ const program = require('commander');
 
 process.env.NODE_PATH = `${__dirname}/../node_modules`;
 
-program.version('0.4.0');
+program.version('0.4.1');
 program.name('kubesecret');
 program.usage('\nGet started by running ./kubesecret get -n <namespace>');
 program.command('get', 'Starts the process of getting secret(s) and performing operations on it');
